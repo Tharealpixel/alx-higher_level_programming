eac1b228e89f673fe624f20179fdb532299b4380
Tharealpixel <medboutahor@gmail.com>
2023-06-12 15:05:44 -0700

    S

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-"""Print list of integers"""
 
+# that retrieves an element from a list like in C.
 
-def print_list_integer(my_list=[]):
-    for i in my_list:
-        print("{:d}".format(i))
+def element_at(my_list, idx):
+    if idx < 0 or idx >= len(my_list):
+        return
+    return my_list[idx]

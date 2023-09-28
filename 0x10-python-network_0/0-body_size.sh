4be37bad5de5d7f66f2e7b392dabc5d0b5430a3c
Tharealpixel <medboutahor@gmail.com>
2023-09-28 13:24:54 +0100

    www

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# script that takes in a URL, request to that URL displays the size of the body
+# script that takes in a URL, request URL displays the size of the body
 curl -s "1$" | wc -c
